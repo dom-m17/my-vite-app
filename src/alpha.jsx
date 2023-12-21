a807a0bd22db3a54c7b48fcd8a55be092cc02f6a
dom-m17 <dominicmaynard00@gmail.com>
2023-12-21 21:08:41 +0000

    First upload of vite-app

---
+++ src/alpha.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function Alpha() {
+
+  return (
+    <>
+        <div>
+            <a src="beta.jsx">Dominic Maynard</a>
+        </div>
+    </>
+  )
+}
+
+export default Alpha
