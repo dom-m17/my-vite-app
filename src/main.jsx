9ea0923b4f986bf9c9b77d3616f629868e71d88f
dom-m17 <123015468+dom-m17@users.noreply.github.com>
2023-12-26 19:29:07 +0000

    Update main.jsx with new comment

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import Alpha from './alpha.jsx'
 
 // New comment 
+// Adding a comment from Github
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
